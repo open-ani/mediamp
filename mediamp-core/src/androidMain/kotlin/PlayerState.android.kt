09d73949f5727f049f09e647c9db1bc30be4533e
Him188 <Him188@mamoe.net>
2024-12-22 13:49:59 +0000

    Move MutableTrackGroup to internal

--- mediamp-core/src/androidMain/kotlin/PlayerState.android.kt
+++ mediamp-core/src/androidMain/kotlin/PlayerState.android.kt
@@ -2,9 +2,9 @@
  * Copyright (C) 2024 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ * Use of this source code is governed by the Apache-2.0 license, which can be found at the following link.
  *
- * https://github.com/open-ani/ani/blob/main/LICENSE
+ * https://github.com/open-ani/mediamp/blob/main/LICENSE
  */
 
 @file:kotlin.OptIn(MediampInternalApi::class)
@@ -52,7 +52,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.openani.mediamp.core.metadata.MutableTrackGroup
+import org.openani.mediamp.core.internal.MutableTrackGroup
 import org.openani.mediamp.core.state.AbstractPlayerState
 import org.openani.mediamp.core.state.MediaPlayerAudioController
 import org.openani.mediamp.core.state.PlaybackState
