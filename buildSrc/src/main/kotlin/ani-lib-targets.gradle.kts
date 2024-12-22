f962ac617db130671f22f8a2c299ffce8e1a3963
Him188 <Him188@mamoe.net>
2024-12-22 13:30:26 +0000

    Remove ani-lib-targets.gradle.kts

--- buildSrc/src/main/kotlin/ani-lib-targets.gradle.kts
+++
@@ -1,6 +0,0 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
-
-(kotlinExtension as? KotlinMultiplatformExtension)?.run {
-    jvm()
-}
