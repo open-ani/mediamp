e89074c97347dbfde0b9b1a7330db05357bf85d7
Him188 <Him188@mamoe.net>
2024-12-29 18:44:49 +0000

    Update copyright

--- .github/workflows/src.main.kts
+++ .github/workflows/src.main.kts
@@ -3,8 +3,7 @@
 /*
  * Copyright (C) 2024 OpenAni and contributors.
  *
- * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
- * Use of this source code is governed by the Apache-2.0 license, which can be found at the following link.
+ * Use of this source code is governed by the GNU GENERAL PUBLIC LICENSE version 3 license, which can be found at the following link.
  *
  * https://github.com/open-ani/mediamp/blob/main/LICENSE
  */
--- mediamp-api/src/commonMain/kotlin/internal/Range.kt
+++ mediamp-api/src/commonMain/kotlin/internal/Range.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * Use of this source code is governed by the GNU GENERAL PUBLIC LICENSE version 3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/mediamp/blob/main/LICENSE
+ */
+
 @file:OptIn(ExperimentalMultiplatform::class)
 
 package org.openani.mediamp.internal
