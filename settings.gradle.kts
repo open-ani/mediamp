ace1e69027eb9140d39d66c89edbf76b34001565
Him188 <Him188@mamoe.net>
2025-02-03 11:30:06 +0000

    Support AVKit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,6 +32,7 @@ include(":mediamp-exoplayer")
 include(":mediamp-exoplayer-compose")
 include(":mediamp-mpv")
 include(":mediamp-mpv-compose")
+include(":mediamp-avkit")
 
 //include(":mediamp-preview")
 include(":mediamp-source-ktxio")
