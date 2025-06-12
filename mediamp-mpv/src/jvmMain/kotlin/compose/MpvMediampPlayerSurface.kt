fd3b13257f2aa022b536dbe366f85e46b383147d
Him188 <Him188@mamoe.net>
2025-06-12 02:15:28 +0100

    fix

--- mediamp-mpv/src/jvmMain/kotlin/compose/MpvMediampPlayerSurface.kt
+++ mediamp-mpv/src/jvmMain/kotlin/compose/MpvMediampPlayerSurface.kt
@@ -11,12 +11,12 @@
 package org.openani.mediamp.mpv.compose
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.layout.onGloballyPositioned
 import org.jetbrains.skiko.GraphicsApi
 import org.jetbrains.skiko.SkikoProperties
 import org.openani.mediamp.InternalMediampApi
@@ -30,16 +30,13 @@ public fun MpvMediampPlayerSurface(
     modifier: Modifier = Modifier,
 ) {
     val contextHolder = remember(mediampPlayer) { MpvRenderContext(mediampPlayer) }
-    var coords: androidx.compose.ui.layout.LayoutCoordinates? = null
     DisposableEffect(Unit) {
         onDispose { contextHolder.release() }
     }
-    Canvas(
-        modifier.onGloballyPositioned { coords = it },
-    ) {
-        val layout = coords ?: return@Canvas
-        if (SkikoProperties.renderApi != GraphicsApi.OPENGL) return@Canvas
+    Canvas(modifier.fillMaxSize()) {
+        if (SkikoProperties.renderApi != GraphicsApi.OPENGL) error("fuck")
         contextHolder.draw(size.width.toInt(), size.height.toInt(), drawContext.canvas.nativeCanvas)
+        println("rendered frame")
     }
 }
 
