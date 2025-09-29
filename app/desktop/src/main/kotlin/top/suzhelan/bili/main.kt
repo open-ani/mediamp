2529b12fa5efcc9f2a9acff6419e19ebf8fa1cee
suzhelan <suzhelan@gmail.com>
2025-09-29 15:26:38 +0800

    Feat Upgrade VLC binaries to 3.0.22-rc2

--- app/desktop/src/main/kotlin/top/suzhelan/bili/main.kt
+++ app/desktop/src/main/kotlin/top/suzhelan/bili/main.kt
@@ -1,12 +1,19 @@
 package top.suzhelan.bili
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.launch
+import org.openani.mediamp.MediampPlayer
 import org.openani.mediamp.compose.MediampPlayerSurface
 import org.openani.mediamp.compose.rememberMediampPlayer
 import org.openani.mediamp.playUri
@@ -22,13 +29,24 @@ fun main() = application {
         val scope = rememberCoroutineScope()
         val mediampPlayer = rememberMediampPlayer()
         scope.launch {
-            mediampPlayer.playUri("https://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8")
+            mediampPlayer.playUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
         }
         Box(modifier = Modifier.fillMaxSize()) {
             MediampPlayerSurface(
                 mediampPlayer = mediampPlayer,
                 modifier = Modifier.fillMaxSize()
             )
+            VideoTime(mediampPlayer)
         }
     }
+}
+
+@Composable
+fun BoxScope.VideoTime(mediampPlayer: MediampPlayer) {
+    val currentPositionMillis by mediampPlayer.currentPositionMillis.collectAsState()
+    val totalDurationMillis by mediampPlayer.mediaProperties.collectAsState()
+    Text(
+        text = "Current position: $currentPositionMillis / ${totalDurationMillis?.durationMillis}",
+        modifier = Modifier.align(Alignment.BottomCenter)
+    )
 }
